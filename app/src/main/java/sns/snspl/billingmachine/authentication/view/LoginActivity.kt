7c3d60a5832522fb057995a75fba2c9656fddec1
AmolWakchaure <ambi.wakchaure@gmail.com>
2019-02-01 16:09:43 +0530

    authentication

---
+++ app/src/main/java/sns/snspl/billingmachine/authentication/view/LoginActivity.kt
@@ -0,0 +1,13 @@
+package sns.snspl.billingmachine.authentication.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import sns.snspl.billingmachine.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
