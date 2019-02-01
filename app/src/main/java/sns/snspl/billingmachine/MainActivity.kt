d1a9c3fe558bf13a8962b9ca0a179139abd7ecfb
Tej <tejas.desai@snsystems.in>
2019-02-01 15:56:58 +0530

    tej

--- app/src/main/java/sns/snspl/billingmachine/MainActivity.kt
+++ app/src/main/java/sns/snspl/billingmachine/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     //amol wakchaure
+    //tejas Desai
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
